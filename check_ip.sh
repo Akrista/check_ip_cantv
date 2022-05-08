f8cd1c3754aacfb171eb8d9d2869cb666b4947ed
Jorge Thomas - Akrista <23145794+Akrista@users.noreply.github.com>
2022-05-08 14:00:42 +0000

    added check if icanhazip doesn't get any ip

--- check_ip.sh
+++ check_ip.sh
@@ -2,14 +2,17 @@
 
 last_ip=$(cat ./.public_ip)
 recent_ip=$(wget -qO - icanhazip.com)
+ip_has_changed="El servidor ha cambiado su IP a {$recent_ip}"
 
 #echo "Revisando IP Publica"
 #sleep 1
-if [[ $last_ip == $recent_ip ]]; then
+if [[ $last_ip == $recent_ip ]] && [[ -n $recent_ip ]]; then
     echo "La IP Publica "+=" no ha cambiado"
-elif [[ $last_ip != $recent_ip ]]; then
+    # sleep 0
+elif [[ $last_ip != $recent_ip ]] && [[ -n $recent_ip ]]; then
     echo "La IP Publica "+=" ha cambiado"
     echo $recent_ip >./.public_ip
+    # bash ./discord.sh --webhook-url="$WEBHOOK" --text "{$ip_has_changed}"
 else
     echo "No hay IP Publica"
 fi
